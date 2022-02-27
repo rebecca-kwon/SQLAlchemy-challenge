ef5bf30d82bb6088f612e2b1826657fc6ff5c1c7
Becca Kwon <kwon.dy12@gmail.com>
2022-02-26 21:22:15 -0600

    updating app.py for start and end date clarification

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/(start)<br/>"
-        f"/api/v1.0/(start)/(end)"
+        f"/api/v1.0/start date (format ""yyyy-mm-dd"")<br/>"
+        f"/api/v1.0/start date/end date (format ""yyyy-mm-dd"")"
     )
 
 
